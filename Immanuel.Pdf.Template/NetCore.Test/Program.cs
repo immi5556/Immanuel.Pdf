99e286415eef58f79faea445636851834fac8d9f
Immanuel <raj@immanuel.co>
2020-05-24 21:11:59 +0530

    changes

---
+++ Immanuel.Pdf.Template/NetCore.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetCore.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = System.IO.Directory.GetCurrentDirectory(); 
+            byte[] file = Resource1.wkhtmltopdf; 
+            CMemoryExecute.Run(file, dir, "-B 25mm -T 25mm --footer-spacing 5 --footer-html 'footer.html' 'http://www.tamil-bible.com/lookup.php?Book=Romans&Chapter=11' lordjesus.pdf");
+        }
+    }
+}
